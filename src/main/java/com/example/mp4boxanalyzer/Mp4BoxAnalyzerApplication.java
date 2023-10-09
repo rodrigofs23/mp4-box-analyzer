56d7aceb0c70251e2f49614d3778184a5427b3e6
Rodrigo Souza <rodrigofs23@hotmail.com>
2023-10-10 00:16:25 +0200

    first commit

---
+++ src/main/java/com/example/mp4boxanalyzer/Mp4BoxAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mp4boxanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp4BoxAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp4BoxAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mp4boxanalyzer/Mp4BoxAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mp4boxanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp4BoxAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
