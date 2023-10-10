f63f16a58ad9563d4e6ae04ae51495594bd2b9e1
Rodrigo Souza <rodrigofs23@hotmail.com>
2023-10-10 15:40:27 +0200

    Add more controller tests

--- src/main/java/com/example/mp4boxanalyzer/BoxController.java
+++ src/main/java/com/example/mp4boxanalyzer/BoxController.java
@@ -3,13 +3,15 @@ package com.example.mp4boxanalyzer;
 import java.util.Collections;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/mp4boxanalyzer")
 public class BoxController {
 
-  @GetMapping("/analyze")
+  @GetMapping()
   public List<Box> analyze(@RequestParam("url") String url) {
     // TODO: Implement this method.
     if (url == null) {
--- src/test/java/com/example/mp4boxanalyzer/BoxControllerTest.java
+++ src/test/java/com/example/mp4boxanalyzer/BoxControllerTest.java
@@ -1,25 +1,51 @@
 package com.example.mp4boxanalyzer;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class BoxControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+  @MockBean private Mp4BoxAnalyzer mp4BoxAnalyzer;
+  @InjectMocks private BoxController boxController;
+
   @Test
   void testAnalyze_withEmptyInput_returnsEmptyList() {
-    BoxController boxController = new BoxController();
     List<Box> boxes = boxController.analyze("");
     assertEquals(0, boxes.size());
   }
 
   @Test
   void testAnalyze_withNullInput_throwsNullPointerException() {
-    BoxController boxController = new BoxController();
-    assertThrows(
-        NullPointerException.class,
-        () -> {
-          List<Box> boxes = boxController.analyze(null);
-        });
+    assertThrows(NullPointerException.class, () -> boxController.analyze(null));
+  }
+
+  @Test
+  void testAnalyze_withInvalidEndpoint_returnsNotFound() throws Exception {
+    mockMvc.perform(get("/mp4boxanalyzer")).andExpect(status().isNotFound());
+  }
+
+  @Test
+  void testAnalyze_withInvalidRequest_returnsBadRequest() throws Exception {
+    mockMvc.perform(get("/api/v1/mp4boxanalyzer")).andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void testAnalyze_withValidEndpoint_returnsOk() throws Exception {
+    mockMvc
+        .perform(get("/api/v1/mp4boxanalyzer").param("url", "valid_url"))
+        .andExpect(status().isOk());
   }
 }
